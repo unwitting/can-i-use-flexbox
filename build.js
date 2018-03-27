6d778d48d1a8147587a2c92e6e3889afa78b57b2
Jack Preston <unwttng@gmail.com>
2018-03-27 15:33:32 +0100

    Working live data

--- build.js
+++ build.js
@@ -1,24 +1,48 @@
 const fs = require("fs-extra");
+const mustache = require("mustache");
 const path = require("path");
 const request = require("request-promise-native");
 
 const BUILD_DIR = path.join(".", "build");
 const SRC_DIR = path.join(".", "src");
 
+const CANIUSE_JSON_CDN =
+  "https://cdn.rawgit.com/Fyrd/caniuse/2f6c1df9/features-json/flexbox.json";
+
 async function build() {
   await ensureBuildDir();
-  await copyHtml();
+  await templateHtml();
 }
 
-async function copyHtml() {
-  await fs.copy(
-    path.join(SRC_DIR, "index.html"),
-    path.join(BUILD_DIR, "index.html")
-  );
+async function templateHtml() {
+  const templateData = await getTemplateData();
+  const indexHtml = (await fs.readFile(
+    path.join(SRC_DIR, "index.mst")
+  )).toString();
+  const rendered = mustache.render(indexHtml, templateData);
+  await fs.writeFile(path.join(BUILD_DIR, "index.html"), rendered);
 }
 
 async function ensureBuildDir() {
   await fs.ensureDir(BUILD_DIR);
+}
+
+async function getCanIUseJson() {
+  return await request(CANIUSE_JSON_CDN).json();
+}
+
+async function getSupportPercentages() {
+  const supportData = await getCanIUseJson();
+  return {
+    global: supportData.usage_perc_y,
+    partial: supportData.usage_perc_y + supportData.usage_perc_a
+  };
+}
+
+async function getTemplateData() {
+  return {
+    support: await getSupportPercentages()
+  };
 }
 
 build().catch(err => {
